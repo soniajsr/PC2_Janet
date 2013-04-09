14aa8f37cb0fbb0764c9ce8940526cffe8724eba
carlos bazan <pacoyx@gmail.com>
2013-04-08 19:58:13 -0500

    pc2
    subiendo

---
+++ IRestFul_Estudiante.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+
+namespace PC2_Janet
+{
+
+      [ServiceContract]
+    public interface  IRestFul_Estudiante
+    {
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/students/", Method = "POST")]
+          Estudiante CreateStudent(Estudiante s);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/students/")]
+        List<Estudiante> GetAllStudent();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/students/search/{query}")]
+        Estudiante GetQuery(string query);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/students/{id}")]
+        Estudiante GetAStudent(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/students/{id}", Method = "PUT")]
+        Estudiante UpdateStudent(string id, Estudiante s);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/students/{id}", Method = "DELETE")]
+        void DeleteStudent(string id);
+
+    }
+
+      #region Entidad Estudiante
+      [DataContract]
+      public class Estudiante
+      {
+          [DataMember]
+          public string ID;
+          [DataMember]
+          public string birthdate;
+          [DataMember]
+          public string firstname;
+          [DataMember]
+          public string idbooster;
+          [DataMember]
+          public string lastname;
+
+      }
+      #endregion
+
+
+}
---
+++ RestFul_Estudiante.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Activation;
+using System.ServiceModel;
+
+
+namespace PC2_Janet
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+
+    public class RestFul_Estudiante : IRestFul_Estudiante
+    {
+
+
+        List<Estudiante> objStudent = new List<Estudiante>();
+        int studentCount = 0;
+
+        public Estudiante CreateStudent(Estudiante s)
+        {
+            s.ID = (++studentCount).ToString();
+            objStudent.Add(s);
+            return s;
+        }
+
+        public List<Estudiante> GetAllStudent()
+        {
+            return objStudent.ToList();
+        }
+
+        public Estudiante GetQuery(string query)
+        {
+            return objStudent.FirstOrDefault(e => e.firstname.Equals(query));
+        }
+
+        public Estudiante GetAStudent(string id)
+        {
+            return objStudent.FirstOrDefault(e => e.ID.Equals(id));
+        }
+
+        public Estudiante UpdateStudent(string id, Estudiante s)
+        {
+            Estudiante p = objStudent.FirstOrDefault(e => e.ID.Equals(id));
+            p.birthdate = s.birthdate;
+            p.firstname = s.firstname;
+            p.lastname = s.lastname;
+            p.idbooster = s.idbooster;
+            return p;
+        }
+
+        public void DeleteStudent(string id)
+        {
+            objStudent.RemoveAll(e => e.ID.Equals(id));
+        }
+    }
+}
